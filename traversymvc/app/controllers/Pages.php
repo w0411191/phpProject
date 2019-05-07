f7583b299853007f0021dfb86d4cd38b655bace9
dong <w0411191@nscc.ca>
2019-05-07 08:04:31 -0400

    18. Base Controller Class

---
+++ traversymvc/app/controllers/Pages.php
@@ -0,0 +1,14 @@
+<?php
+  class Pages extends Controller {
+    public function __construct(){
+
+    }
+
+    public function index(){
+      $this->view('hello');
+    }
+
+    public function about($id){
+      echo $id;
+    }
+  }
